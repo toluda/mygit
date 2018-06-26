b34bc15fafc48e037374b51caa2cabc62659a369
toluda <toluda@gmail.com>
2018-06-26 17:54:29 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
+/**
+ * Class Calculate
+ * @author kravchenko
+ * @since 26.06.2018 
+*/
 package ru.job4j.calculate;
 
 public class Calculate {
+	/**
+	* Main
+	* @param args - args. 
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author kravchenko (mailto:toluda@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
