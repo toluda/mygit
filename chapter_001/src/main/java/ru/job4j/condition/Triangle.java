e5853ec25910b6bd06b442fa36546d5dbe113efd
toluda <toluda@gmail.com>
2018-06-29 02:49:26 +0300

    4.3. Вычисление площади треугольника [#9461]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,55 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double period(double ab, double ac, double bc) {
+        return (ab + ac + bc) * 0.5;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * @return Вернуть прощадь, если треугольник существует или -1, если треугольника нет.
+     */
+    public double area() {
+        double rsl = -1; // мы устанавливаем значение -1, так как может быть что треугольника нет. Это значение говорит о том. что треугольника нет.
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            // написать формулу для расчета площади треугольника.
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));  //вместо многоточия нужно написать формулу Герона с подставленными переменные ab, ac, dc, p.
+            // Для извлечение квадратного корня надо использовать метод Math.sqrt()
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     *
+     * @param ab Длина от точки a b.
+     * @param ac Длина от точки a c.
+     * @param bc Длина от точки b c.
+     * @return
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        if (ab <= 0 || ac <= 0 || bc <= 0) {
+            return false;
+        } else if (ab >= ac + bc || ac >= ab + bc || bc >= ab + ac) {
+            return false;
+        }
+        return true;
+    }
+}
