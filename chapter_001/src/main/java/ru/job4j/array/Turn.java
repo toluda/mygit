d2467979c9a731405b35b1bc071e351ba80df015
toluda <toluda@gmail.com>
2018-06-30 21:08:17 +0300

    6.0. Заполнить массив степенями чисел. [#33488]v2

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -3,14 +3,9 @@ package ru.job4j.array;
 public class Turn {
     public int[] turn(int[] array) {
         int temp;
-        int k;
-        if ((array.length % 2) == 0) {
-         k = array.length / 2;
-        } else {
-            k = (array.length - 1) / 2 + 1;
-        }
 
-        for (int i = 0; i < k; i++) {
+
+        for (int i = 0; i < array.length / 2; i++) {
             temp = array[i];
             array[i] = array[array.length - i - 1];
             array[array.length - i - 1] = temp;
