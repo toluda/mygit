33abbebb25e0f561ab0b5675c570430a84b605b9
toluda <toluda@gmail.com>
2018-07-01 18:13:37 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям. [#53859]

---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixCheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true},
+                {false, true, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true},
+                {false, false, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataMonoByTrueThenTrueSize4() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true, true},
+                {false, true, true, true},
+                {true, true, true, true},
+                {true, false, true, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalseSize4() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false, true},
+                {false, false, true, false},
+                {true, false, true, true},
+                {true, false, true, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
